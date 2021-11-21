affa41bc34aabea84322f8d3af8eb9327f482fb0
RISHIKA KESHARWANI <61060542+rishika8119@users.noreply.github.com>
2021-11-21 11:37:41 +0530

    BFS for disconnected graph

---
+++ graph/BFS_disconnected.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+void addedge(vector<int> adj[] ,int u,int v)
+{
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+void BFS(vector<int> adj[],int s,bool visited[])
+{
+    
+    queue<int> q;
+    visited[s]=true;
+    q.push(s);
+
+    while(q.empty()==false)
+    {
+        int u=q.front();
+        q.pop();
+        cout<<u<<" ";
+        for(auto &x:adj[u])
+        {
+            if(visited[x]==false)
+            {
+                visited[x]=true;
+                q.push(x);
+            }
+        }
+    }
+
+}
+void BFSdis(vector<int> adj[],int V)
+{
+    bool visited[V];
+    for(int i=0;i<V;i++)
+    {
+        visited[i]=false;
+    }
+
+ 
+  for(int i=0;i<V;i++)
+  {
+    if(visited[i]==false)
+    {
+     BFS(adj,i,visited);
+    }
+  }
+}
+int main()
+{
+  ///////
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  #ifndef ONLINE_JUDGE
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+  #endif
+  ///////
+
+  int V;
+  cin>>V;
+  vector<int> adj[V];
+  	addedge(adj,0,1); 
+	addedge(adj,0,2); 
+	addedge(adj,2,3); 
+	addedge(adj,1,3); 
+	addedge(adj,4,5);
+	addedge(adj,5,6);
+	addedge(adj,4,6);
+
+
+   BFSdis(adj,V);
+  return 0;
+}
